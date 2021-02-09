145db73a1a9386d70610322953bc11258e650a03
Nathan Davis <u0777729@utah.edu>
2021-02-09 09:21:02 -0700

    Added Variant testing for JS diff detection

--- variant_tester.py
+++ variant_tester.py
@@ -1,10 +1,13 @@
-from layout_tester import test_combination
+from web_page_creation.create import save_as_web_page
+from html_file_generator import get_file_path, remove_file
+from file_config import FileConfig
+from layout_tester import run_test_using_js_diff_detect, test_combination
 from webdrivers import chrome
 from test_subject import TestSubject
 
 
 
-def get_variant(webdriver, bug_gone, description):
+def get_variant(webdriver, bug_gone, description, diff_method="Python"):
     browser_version = webdriver.capabilities['browserVersion']
     driver_version = webdriver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]
     window_size = webdriver.get_window_size()
@@ -15,6 +18,7 @@ def get_variant(webdriver, bug_gone, description):
         "browser_version": browser_version,
         "browser_driver_version": driver_version,
         "window_size": window_size,
+        "diff_method": diff_method
     }
 
 
@@ -42,5 +46,21 @@ def test_variants(test_subject: TestSubject):
     bug_gone, *_ = test_combination(chrome_webdriver, test_subject)
     variants.append(get_variant(chrome_webdriver, bug_gone, description))
     chrome_webdriver.close()
+
+    # Using JS Change Detection
+    description = "JavaScript Difference Detection"
+    chrome_webdriver = chrome.getWebDriver()
+
+    file_config = FileConfig()
+    test_filepath, test_filename = get_file_path(file_config.layout_file_dir)
+    save_as_web_page(test_subject, test_filepath)
+    test_web_page = f"http://localhost:8000/{file_config.relative_url_path}/{test_filename}"
+    differences = run_test_using_js_diff_detect(test_web_page, chrome_webdriver, slow=True)
+    bug_gone = differences is None
+
+    variants.append(get_variant(chrome_webdriver, bug_gone, description, diff_method="JavaScript"))
+    remove_file(test_filepath)
+    chrome_webdriver.close()
+
 
     return variants
