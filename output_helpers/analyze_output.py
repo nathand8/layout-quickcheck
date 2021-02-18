2d79fe771c7ceb291e90fc34bfde669dceaf5ba2
Nathan Davis <u0777729@utah.edu>
2021-02-18 12:38:02 -0700

    Enhanced script to find analyze bugs

--- output_helpers/analyze_output.py
+++ output_helpers/analyze_output.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import collections.abc
 
 DEFAULT_BUG_REPORT_DIR = './bugreportfiles'
 DEFAULT_JSON_FILENAME = 'data.json'
@@ -11,15 +12,30 @@ def collect_json_reports(bug_report_dir, json_filename):
     for json_filepath in json_filepaths:
         with open(json_filepath, 'r') as f:
             reports.append(json.loads(f.read()))
-    
     return reports
 
+def get_matches(bugs, v_match):
+    is_dict = lambda o: isinstance(o, collections.abc.Mapping)
+    correct_format = lambda bug: is_dict(bug) and "variants" in bug and is_dict(bug["variants"]) and "Test Variant Summary" in bug["variants"] and is_dict(bug["variants"]["Test Variant Summary"])
+    get_variants = lambda bug: bug["variants"]["Test Variant Summary"]
+    return [bug for bug in bugs if correct_format(bug) and v_match(get_variants(bug))]
+
+def count_matches(bugs, v_match):
+    return len(get_matches(bugs, v_match))
 
 if __name__ == "__main__":
     bug_report_dir = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_BUG_REPORT_DIR
 
-    reports = collect_json_reports(bug_report_dir, DEFAULT_JSON_FILENAME)
-    print(json.dumps(reports[0], indent=4))
+    bugs = collect_json_reports(bug_report_dir, DEFAULT_JSON_FILENAME)
+
+    print("Total Total:", len(bugs))
+    print("Total:", count_matches(bugs, lambda v: True))
+    print("Default:", count_matches(bugs, lambda v: v["Default Variant"]))
+    print("Slow:", count_matches(bugs, lambda v: v["Slow - Forced Waits"]))
+    print("Smaller Window:", count_matches(bugs, lambda v: v["Smaller Window Size"]))
+    print("Larger Window:", count_matches(bugs, lambda v: v["Larger Window Size"]))
+    print("JS Diff:", count_matches(bugs, lambda v: v["JavaScript Difference Detection"]))
+    print("Firefox:", count_matches(bugs, lambda v: v["Firefox Browser"]))
 
 
 
