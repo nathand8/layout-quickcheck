df134a80eb609e52ba7c7a59766f8100267bf334
Pavel Panchekha <me@pavpanchekha.com>
2021-03-08 11:10:00 -0700

    Implement a nightly

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+setup:
+	pip3 install -r requirements.txt
+
+nightly:
+	bash infra/nightly.sh
---
+++ infra/nightly.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+START=`date "+%m/%d/%Y %H:%M:%S"`
+python3 src/compare.py -t 10000
+COUNT=$(find bugreportfiles/* -maxdepth 0 -type d -newermt "$START" | wc -l)
+
+if command -v nightly-results &>/dev/null; then
+    nightly-results bugs "$COUNT"
+fi
