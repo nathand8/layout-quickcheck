6cbff4b84472aa58e5c52556ddb31ba23b223c5f
Pavel Panchekha <me@pavpanchekha.com>
2021-03-08 11:19:39 -0700

    More path attempts

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -2,8 +2,8 @@
 set -e -x
 
 START=`date "+%m/%d/%Y %H:%M:%S"`
-export CHROME_DRIVER_PATH=`which chomedriver`
-export FIREFOX_DRIVER_PATH=`which geckodriver`
+export CHROME_DRIVER_PATH=/home/p92/bin/chromedriver
+export FIREFOX_DRIVER_PATH=/home/p92/bin/geckodriver
 python3 src/compare.py -t 10000
 COUNT=$(find bugreportfiles/* -maxdepth 0 -type d -newermt "$START" | wc -l)
 
