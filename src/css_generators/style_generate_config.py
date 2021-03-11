55d924368350e5d63b6af8b73a15fc7b90d8b649
Nathan Davis <u0777729@utah.edu>
2021-03-11 10:27:34 -0700

    Added Style Generate Config file

--- src/css_generators/style_generate_config.py
+++ src/css_generators/style_generate_config.py
@@ -1,3 +1,10 @@
+DEFAULT_STYLE_WEIGHT = 10
+
+def _weightToProbability(weight):
+    return weight/100
+
+def _bound(low, high, value):
+    return max(low, min(high, value))
 
 class StyleGenerateConfig():
 
@@ -5,4 +12,7 @@ class StyleGenerateConfig():
         self.style_weights = config["style_weights"]
     
     def getStyleProbability(self, style_name):
+        weight = self.style_weights.get(style_name, DEFAULT_STYLE_WEIGHT)
+        weight = _bound(0, 100, weight)
+        return _weightToProbability(weight)
         
--- test/test_style_generate_config.py
+++ test/test_style_generate_config.py
@@ -1,20 +1,28 @@
 import unittest
+from src.css_generators.style_generate_config import StyleGenerateConfig
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_getStyleProbability(self):
+        config = {
+            "style_weights": {
+                "margin-top": -10,
+                "margin-bottom": 0,
+                "margin-left": 200,
+                "margin-right": 100,
+                "border-left": 15,
+                # "border-right": unset
+            }
+        }
+        sgc = StyleGenerateConfig(config)
+        print(sgc.getStyleProbability("margin-top"))
+        self.assertAlmostEqual(0.00, sgc.getStyleProbability("margin-top"))
+        self.assertAlmostEqual(0.00, sgc.getStyleProbability("margin-bottom"))
+        self.assertAlmostEqual(1.00, sgc.getStyleProbability("margin-left"))
+        self.assertAlmostEqual(1.00, sgc.getStyleProbability("margin-right"))
+        self.assertAlmostEqual(0.15, sgc.getStyleProbability("border-left"))
+        self.assertAlmostEqual(0.10, sgc.getStyleProbability("border-right"))
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
