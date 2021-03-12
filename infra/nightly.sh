4f4175c6d98209f4ec93c5d641fa601c6b773d91
Nathan Davis <u0777729@utah.edu>
2021-03-12 15:33:08 -0700

    Simplifying nightly run - removing space in label

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -13,6 +13,6 @@ python3 src/compare.py -t 10000 -c ./config/test-grid.config.json
 GRID_COUNT=$(find bugreportfiles/* -maxdepth 0 -type d -newermt "$START" | wc -l)
 
 if command -v nightly-results &>/dev/null; then
-    nightly-results 'Default Profile' "$DEFAULT_COUNT"
-    nightly-results 'Grid Profile' "$GRID_COUNT"
+    nightly-results Default "$DEFAULT_COUNT"
+    nightly-results Grid "$GRID_COUNT"
 fi
