2c15444e136e949415cefa958a425ea9e2e4b217
Nathan Davis <u0777729@utah.edu>
2021-04-28 13:02:56 -0600

    Added searching and filtering

--- web/ui/src/BugList/BugList.js
+++ web/ui/src/BugList/BugList.js
@@ -3,12 +3,15 @@ import _ from "lodash";
 
 import './BugList.css';
 
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip'
 import BugDetails from '../BugDetails/BugDetails';
-import { FaBug, FaExclamationTriangle } from 'react-icons/fa';
+import { FaBug, FaChrome, FaExclamationTriangle, FaFirefox, FaTimes } from 'react-icons/fa';
 
 export function displayStyleList(styles) {
   const ignore = new Set(["background-color", "min-width", "min-height"]);
@@ -17,11 +20,29 @@ export function displayStyleList(styles) {
   return _.join(styles, ', ');
 }
 
+export function applySearch(searchStr, bugs) {
+  let groups = searchStr.split(/[ ,]+/); // Split by whitespace
+  let filters = groups
+    .filter(group => group.length > 0)
+    .map((group) => {
+      if (group.startsWith("type:")) {
+        return (bug) => (bug.bug_type.toLowerCase().includes(group.replace("type:", "").toLowerCase()));
+      } else if (group.startsWith("seen:")) {
+        return (bug) => (bug.variants["Test Variant Details"].filter(variant => variant.bug_detected).map(variant => variant.description).join(', ').toLowerCase().includes(group.replace("seen:", "").toLowerCase()))
+      } else {
+        return (bug) => (bug.styles_used_string.includes(group));
+      }
+    })
+  return bugs.filter(bug => filters.every(filter => filter(bug)));
+}
+
 export default function BugList(props) {
 
   const bugsAPI = "http://localhost:5000/bugs";
 
+  const [searchStr, setSearchStr] = useState("");
   const [bugs, setBugs] = useState([]);
+  const [filteredBugs, setFilteredBugs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -30,17 +51,50 @@ export default function BugList(props) {
       .then(response => {
         setBugs(response)
         setIsLoading(false);
-        console.log(response);
       })
       .catch(error => console.error(error));
   }, []); // Only run once when component loads
 
+  useEffect(() => {
+    if (bugs) {
+      if (searchStr) {
+        setFilteredBugs(applySearch(searchStr, bugs));
+      } else {
+        setFilteredBugs(bugs);
+      }
+    } else {
+      setFilteredBugs([]);
+    }
+  }, [searchStr, bugs])
+
   return (
     <div className="BugList">
-      {isLoading && <p>Loading Bugs...</p>}
-      <Accordion defaultActiveKey="1">
 
-        {bugs.map((bug, index) => {
+      <InputGroup className="search-container">
+        <InputGroup.Prepend>
+          <OverlayTrigger delay={{show: 500, hide: 50}} placement="bottom" overlay={<Tooltip>Clear Search</Tooltip>}>
+            <Button variant="outline-secondary" onClick={e => setSearchStr("")}><FaTimes /></Button>
+          </OverlayTrigger>
+          <OverlayTrigger delay={{show: 500, hide: 50}} placement="bottom" overlay={<Tooltip>Page Crash Bugs</Tooltip>}>
+            <Button variant="outline-secondary" onClick={e => setSearchStr(searchStr + " type:Crash")}><FaExclamationTriangle /></Button>
+          </OverlayTrigger>
+          <OverlayTrigger delay={{show: 500, hide: 50}} placement="bottom" overlay={<Tooltip>Underinvalidation Bugs</Tooltip>}>
+            <Button variant="outline-secondary" onClick={e => setSearchStr(searchStr + " type:Invalidation")}><FaBug /></Button>
+          </OverlayTrigger>
+          <OverlayTrigger delay={{show: 500, hide: 50}} placement="bottom" overlay={<Tooltip>Chrome Bugs</Tooltip>}>
+            <Button variant="outline-secondary" onClick={e => setSearchStr(searchStr + " seen:Chrome")}><FaChrome /></Button>
+          </OverlayTrigger>
+          <OverlayTrigger delay={{show: 500, hide: 50}} placement="bottom" overlay={<Tooltip>Firefox Bugs</Tooltip>}>
+            <Button variant="outline-secondary" onClick={e => setSearchStr(searchStr + " seen:Firefox")}><FaFirefox /></Button>
+          </OverlayTrigger>
+        </InputGroup.Prepend>
+        <FormControl aria-describedby="basic-addon1" value={searchStr} onChange={event => setSearchStr(event.target.value)}/>
+      </InputGroup>
+
+      {isLoading && <p>Loading Bugs...</p>}
+
+      <Accordion defaultActiveKey="1">
+        {filteredBugs.map((bug, index) => {
           let key = index + 1;
           let icon;
           let tooltip;
