4afbb50f2579973c7ae5895c7c3641d557ce2bfd
Nathan Davis <u0777729@utah.edu>
2021-04-28 11:22:22 -0600

    Added tooltips for bug and crash indicators

--- web/ui/src/BugList/BugList.js
+++ web/ui/src/BugList/BugList.js
@@ -5,7 +5,10 @@ import './BugList.css';
 
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip'
 import BugDetails from '../BugDetails/BugDetails';
+import { FaBug, FaExclamationTriangle } from 'react-icons/fa';
 
 export function displayStyleList(styles) {
   const ignore = new Set(["background-color", "min-width", "min-height"]);
@@ -39,10 +42,24 @@ export default function BugList(props) {
 
         {bugs.map((bug, index) => {
           let key = index + 1;
+          let icon;
+          let tooltip;
+          if (bug.bug_type.includes("Crash")) {
+            icon = <FaExclamationTriangle />;
+            tooltip = "Page Crash";
+          } else {
+            icon = <FaBug />
+            tooltip = "Under Invalidation Bug";
+          }
           return (
           <Card key={index}>
             <Accordion.Toggle as={Card.Header} eventKey={key} className="clickable">
-              {bug.bug_type} - {displayStyleList(bug.styles_used)}
+              <span className="icon">
+                <OverlayTrigger delay={{show: 500, hide: 50}} placement="bottom" overlay={<Tooltip>{tooltip}</Tooltip>}>
+                  {icon}
+                </OverlayTrigger>
+              </span> 
+              {displayStyleList(bug.styles_used)}
             </Accordion.Toggle>
             <Accordion.Collapse eventKey={key}>
               <Card.Body><BugDetails data={bug}/></Card.Body>
