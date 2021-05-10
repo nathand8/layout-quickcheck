454186973b1aab958be6efa7dfd93364301d96e7
Nathan Davis <u0777729@utah.edu>
2021-05-10 11:45:19 -0600

    Updated folder to search in for nightly bug runs

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -3,11 +3,11 @@ set -e -x
 
 START=`date "+%m/%d/%Y %H:%M:%S"`
 python3 src/compare.py -t 10000
-DEFAULT_COUNT=$(find bugreportfiles/* -maxdepth 0 -type d -newermt "$START" | wc -l)
+DEFAULT_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
 
 START=`date "+%m/%d/%Y %H:%M:%S"`
 python3 src/compare.py -t 10000 -c ./config/preset-test-grid.config.json
-GRID_COUNT=$(find bugreportfiles/* -maxdepth 0 -type d -newermt "$START" | wc -l)
+GRID_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
 
 if command -v nightly-results &>/dev/null; then
     nightly-results Default "$DEFAULT_COUNT"
