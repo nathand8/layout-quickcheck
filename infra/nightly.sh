e368d3b1a56de294c4280ce1ebb3e5cc54f2b898
Pavel Panchekha <me@pavpanchekha.com>
2021-06-29 17:47:07 -0600

    Don't run default profile

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -2,14 +2,10 @@
 set -e -x
 
 START=`date "+%m/%d/%Y %H:%M:%S"`
-python3 src/compare.py -t 10000
-DEFAULT_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
-
-START=`date "+%m/%d/%Y %H:%M:%S"`
 python3 src/compare.py -t 10000 -c ./config/preset-test-grid.config.json
 GRID_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
 
 if command -v nightly-results &>/dev/null; then
-    nightly-results Default "$DEFAULT_COUNT"
+    nightly-result "Chrome Version" `google-chrome --version`
     nightly-results Grid "$GRID_COUNT"
 fi
