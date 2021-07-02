add6ca15c5b58b370301a7e9e1e6d74572011c2d
Nathan Davis <ndavis@simplifile.com>
2021-07-02 08:15:25 -0600

    Allow searching by browser version

--- web/ui/src/Search/Search.js
+++ web/ui/src/Search/Search.js
@@ -1,16 +1,47 @@
+function typeMatch(bug, s) {
+  return bug.bug_type.toLowerCase().includes(s.toLowerCase());
+}
+
+function variantDescriptionMatch(bug, s) {
+  return bug.variants["Test Variant Details"]
+          .filter(variant => variant.bug_detected)
+          .map(variant => variant.description.replace(/\s+/g, '')) // remove all whitespace
+          .join(', ')
+          .toLowerCase()
+          .includes(s.toLowerCase());
+}
+
+function styleContains(bug, s) {
+  return bug.styles_used_string.toLowerCase().includes(s.toLowerCase());
+}
+
+function variantBrowserVersionMatch(bug, s) {
+  return bug.variants["Test Variant Details"]
+          .filter(variant => variant.bug_detected)
+          .map(variant => variant.browser_version)
+          .join(', ')
+          .toLowerCase()
+          .includes(s.toLowerCase());
+}
+
 
 export function applySearch(searchStr, bugs) {
   let groups = searchStr.split(/[ ,]+/); // Split by whitespace
   let filters = groups
     .filter(group => group.length > 0)
     .map((group) => {
+
       if (group.startsWith("type:")) {
-        return (bug) => (bug.bug_type.toLowerCase().includes(group.replace("type:", "").toLowerCase()));
+        return (bug) => typeMatch(bug, group.replace("type:", ""));
       } else if (group.startsWith("seen:")) {
-        return (bug) => (bug.variants["Test Variant Details"].filter(variant => variant.bug_detected).map(variant => variant.description).join(', ').toLowerCase().includes(group.replace("seen:", "").toLowerCase()))
+        return (bug) => variantDescriptionMatch(bug, group.replace("seen:", ""));
+      } else if (group.startsWith("version:")) {
+        return (bug) => variantBrowserVersionMatch(bug, group.replace("version:", ""));
       } else {
-        return (bug) => (bug.styles_used_string.includes(group));
+        // Default case - search the style string
+        return (bug) => styleContains(bug, group);
       }
     })
-  return bugs.filter(bug => filters.every(filter => filter(bug)));
+
+  return bugs.filter(bug => filters.every(f => f(bug)));
 }
