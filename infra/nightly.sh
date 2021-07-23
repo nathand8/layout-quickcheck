1249b81c7e07400139e94eca428fa37c2c3ea457
Pavel Panchekha <me@pavpanchekha.com>
2021-07-23 10:29:33 -0600

    Try adding a Firefox run

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e -x
 
+PATH="$HOME/firefox:$PATH"
+
 START=`date "+%m/%d/%Y %H:%M:%S"`
 python3 src/compare.py -t 10000 -c ./config/preset-test-grid.config.json
-GRID_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
+CHROME_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
+
+START=`date "+%m/%d/%Y %H:%M:%S"`
+python3 src/compare.py -t 10000 -c ./config/nightly-firefox.json
+FFX_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
 
 if command -v nightly-results &>/dev/null; then
-    nightly-results "Chrome Version" `google-chrome --version`
-    nightly-results Grid "$GRID_COUNT"
+    nightly-results $(google-chrome --version) "$CHROME_COUNT"
+    nightly-results $(firefox --version) "$FFX_COUNT"
 fi
