d227a2b85b791ba713dc807df80cdfa3b1e91d83
Nathan Davis <ndavis@simplifile.com>
2021-08-11 22:50:43 -0600

    Added grizzly adapter and made some initial implementations

--- src/web_page_creation/create.py
+++ src/web_page_creation/create.py
@@ -1,6 +1,8 @@
+from enum import Enum, unique
 from run_subject import RunSubject
 from web_page_creation.javascript_with_debugging_tools.create import create as js_with_debugging
 from web_page_creation.javascript_minimal.create import create as js_minimal
+from web_page_creation.javascript_grizzly.create import create as js_grizzly
 from web_page_creation.html_body.create import create as html_body
 from web_page_creation.util import formatWithIndent
 
@@ -22,18 +24,27 @@ html_template = """
 </html>
 """
 
-def html_string(run_subject: RunSubject, use_minimal_js=False):
+@unique
+class JsVersion(Enum):
+    DEBUGGING = 0
+    MINIMAL = 1
+    GRIZZLY = 2
+
+
+def html_string(run_subject: RunSubject, js_version=JsVersion.DEBUGGING):
 
     body_string = html_body(run_subject)
-    if use_minimal_js:
-        js_string = js_minimal(run_subject)
-    else:
+    if js_version == JsVersion.DEBUGGING:
         js_string = js_with_debugging(run_subject)
+    elif js_version == JsVersion.MINIMAL:
+        js_string = js_minimal(run_subject)
+    elif js_version == JsVersion.GRIZZLY:
+        js_string = js_grizzly(run_subject)
 
     return formatWithIndent(html_template, js_string=js_string, body_string=body_string)
 
 
 def save_as_web_page(run_subject: RunSubject, file_path, use_minimal_js=False):
     with open(file_path, 'w') as file:
-        file.write(html_string(run_subject, use_minimal_js))
+        file.write(html_string(run_subject, JsVersion.MINIMAL if use_minimal_js else JsVersion.DEBUGGING))
 
