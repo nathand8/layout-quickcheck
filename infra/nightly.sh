683189459f58d1be732bb726032930e6af6ca3d6
Nathan Davis <ndavis@simplifile.com>
2021-08-22 09:34:39 -0600

    Updated nightly script to run correct script after refactors

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -4,11 +4,11 @@ set -e -x
 PATH="$HOME/firefox:$PATH"
 
 START=`date "+%m/%d/%Y %H:%M:%S"`
-python3 src/compare.py -t 10000 -c ./config/preset-test-grid.config.json
+python3 src/lqc_selenium/runner.py -t 10000 -c ./config/preset-test-grid.config.json
 CHROME_COUNT=$(find bug_reports -maxdepth 1 -type d -newermt "$START" | wc -l)
 
 START=`date "+%m/%d/%Y %H:%M:%S"`
-python3 src/compare.py -t 10000 -c ./config/nightly-firefox.json
+python3 src/lqc_selenium/runner.py -t 10000 -c ./config/nightly-firefox.json
 FFX_COUNT=$(find bug_reports -maxdepth 1 -type d -newermt "$START" | wc -l)
 
 if command -v nightly-results &>/dev/null; then
