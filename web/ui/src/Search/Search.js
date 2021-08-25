7d75867f73bad9bfa9792736fc02e68214ada34c
Nathan Davis <nathand120@gmail.com>
2021-08-25 11:26:08 -0600

    Added ability to search NOT criteria

--- web/ui/src/Search/Search.js
+++ web/ui/src/Search/Search.js
@@ -1,23 +1,25 @@
-function typeMatch(bug, s) {
-  return bug.bug_type.toLowerCase().includes(s.toLowerCase());
+function typeMatch(bug, s, reverse = false) {
+  let result = bug.bug_type.toLowerCase().includes(s.toLowerCase());
+  return reverse ? !result : result;
 }
 
-function variantDescriptionMatch(bug, s) {
+function variantDescriptionMatch(bug, s, reverse = false) {
   return bug.variants["Test Variant Details"]
-          .filter(variant => variant.bug_detected)
+          .filter(v => reverse ? !v.bug_detected : v.bug_detected)
           .map(variant => variant.description.replace(/\s+/g, '')) // remove all whitespace
           .join(', ')
           .toLowerCase()
           .includes(s.toLowerCase());
 }
 
-function styleContains(bug, s) {
-  return bug.styles_used_string.toLowerCase().includes(s.toLowerCase());
+function styleContains(bug, s, reverse = false) {
+  let result = bug.styles_used_string.toLowerCase().includes(s.toLowerCase());
+  return reverse ? !result : result;
 }
 
-function variantBrowserVersionMatch(bug, s) {
+function variantBrowserVersionMatch(bug, s, reverse = false) {
   return bug.variants["Test Variant Details"]
-          .filter(variant => variant.bug_detected)
+          .filter(v => reverse ? !v.bug_detected : v.bug_detected)
           .map(variant => variant.browser_version)
           .join(', ')
           .toLowerCase()
@@ -33,10 +35,16 @@ export function applySearch(searchStr, bugs) {
 
       if (group.startsWith("type:")) {
         return (bug) => typeMatch(bug, group.replace("type:", ""));
+      } else if (group.startsWith("!type:")) {
+        return (bug) => typeMatch(bug, group.replace("!type:", ""), true);
       } else if (group.startsWith("seen:")) {
         return (bug) => variantDescriptionMatch(bug, group.replace("seen:", ""));
+      } else if (group.startsWith("!seen:")) {
+        return (bug) => variantDescriptionMatch(bug, group.replace("!seen:", ""), true);
       } else if (group.startsWith("version:")) {
         return (bug) => variantBrowserVersionMatch(bug, group.replace("version:", ""));
+      } else if (group.startsWith("!version:")) {
+        return (bug) => variantBrowserVersionMatch(bug, group.replace("!version:", ""), true);
       } else {
         // Default case - search the style string
         return (bug) => styleContains(bug, group);
