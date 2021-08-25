0607a20d3a9fbc1afa6c3fdecf7794cf9c905f71
Nathan Davis <nathand120@gmail.com>
2021-08-25 11:26:06 -0600

    Fixed typo in nightly run file

--- infra/nightly.sh
+++ infra/nightly.sh
@@ -6,6 +6,6 @@ python3 src/compare.py -t 10000 -c ./config/preset-test-grid.config.json
 GRID_COUNT=$(find bug_reports/* -maxdepth 0 -type d -newermt "$START" | wc -l)
 
 if command -v nightly-results &>/dev/null; then
-    nightly-result "Chrome Version" `google-chrome --version`
+    nightly-results "Chrome Version" `google-chrome --version`
     nightly-results Grid "$GRID_COUNT"
 fi
