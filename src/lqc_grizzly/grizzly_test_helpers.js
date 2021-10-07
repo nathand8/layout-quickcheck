a8cac765ba0ee4caa59c76ba69a0a17e87743ae4
Nathan Davis <nathand120@gmail.com>
2021-10-07 10:46:12 -0600

    Fix indentation on JS file

--- src/lqc_grizzly/grizzly_test_helpers.js
+++ src/lqc_grizzly/grizzly_test_helpers.js
@@ -6,33 +6,33 @@ function test_bug_and_report() {
 
     if (dimensionsDiffer && dimensionsDiffer.length > 0) {
 
-        // report the bug to the test runner
-        fetch("/found")
-        .finally(() => {
-            finish_test(dimensionsDiffer);
-        })
+      // report the bug to the test runner
+      fetch("/found")
+      .finally(() => {
+        finish_test(dimensionsDiffer);
+      })
 
     } else {
-        finish_test()
+      finish_test()
     }
 
   // If "recreateTheProblem" doesn't exist, something went wrong
   } else {
-      console.error("Missing crucial function 'recreateTheProblem'. This should be provided by LQC");
+    console.error("Missing crucial function 'recreateTheProblem'. It should be provided by LQC");
   }
 }
 
 function log_bug_details(dimensionsDiffer) {
 
-    // Add details for easier triaging
-    logOutput = "\\nLQC Results:"
-    for (el of dimensionsDiffer) {
-        logOutput += "\\nConflicting dimensions for element " + el.element;
-        logOutput += "\\n    Dimensions after reload: " + JSON.stringify(post_reload_dims);
-        logOutput += "\\n    Dimensions after modify: " + JSON.stringify(post_modify_dims);
-    }
-    logOutput += "\\nEND of LQC Results\\n"
-    try {
-        window.dump(logOutput);
-    } catch { }
+  // Add details for easier triaging
+  logOutput = "\\nLQC Results:"
+  for (el of dimensionsDiffer) {
+    logOutput += "\\nConflicting dimensions for element " + el.element;
+    logOutput += "\\n    Dimensions after reload: " + JSON.stringify(post_reload_dims);
+    logOutput += "\\n    Dimensions after modify: " + JSON.stringify(post_modify_dims);
+  }
+  logOutput += "\\nEND of LQC Results\\n"
+  try {
+    window.dump(logOutput);
+  } catch { }
 }
